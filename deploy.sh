426d08dfcb9d4a976b20debaa787b474695012a6
Operanda-lab <olivierperez81@gmail.com>
2026-02-20 10:22:51 +0100

    Ajout section PostGIS

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Vérifie qu'un message est fourni
+if [ -z "$1" ]; then
+  echo "Tu dois fournir un message de commit."
+  echo "Usage : ./deploy.sh \"Mon message\""
+  exit 1
+fi
+
+# Ajout des fichiers
+git add *
+
+# Commit avec le message passé en argument
+git commit -m "$1"
+
+# Push
+git push
+
+# Déploiement MkDocs
+mkdocs gh-deploy
